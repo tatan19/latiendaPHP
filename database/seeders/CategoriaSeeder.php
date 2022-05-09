ae89297f84fb293a0a0963e69d5a644e558e79b1
Your Name <you@example.com>
2022-05-09 11:22:29 -0500

    migraciones

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -2,20 +2,18 @@
 
 namespace Database\Seeders;
 use App\Models\Categoria;
-use App\Models\Marca;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CategoriaSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        Marca::factory(50)->create();
         Categoria::factory(50)->create();
     }
 }
